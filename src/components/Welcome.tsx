83b4d75e3567e3f27929386fff13fb3185a73f19
abdus satter <asatter833@gmail.com>
2024-06-30 05:38:02 +0600

    tailwindcss installed and setup

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="font-semibold mt-10">
+      Welcome Asatter833! You have 10 works to complete.
+    </div>
+  );
+};
+
+export default Welcome;
