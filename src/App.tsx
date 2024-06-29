386fa745fcc79478db625a1da46582cfeff4e59a
abdus satter <asatter833@gmail.com>
2024-06-30 05:42:02 +0600

    Props in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Welcome from "./components/Welcome";
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <Welcome name="Abdus Satter" workCount={10} />
     </div>
   );
 }
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Welcome = () => {
+type WelcomeProps = {
+  name: string;
+  workCount: number;
+};
+
+const Welcome = (props: WelcomeProps) => {
   return (
     <div className="font-semibold mt-10">
-      Welcome Asatter833! You have 10 works to complete.
+      Welcome {props.name}! You have {props.workCount} works to complete.
     </div>
   );
 };
